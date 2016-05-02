f990e58d176141d17ce6916f049f1cad1fba2e5a
Gearnode <bfrimin@student.42.fr>
2016-05-02 16:31:53 +0200

    Disable middleware by default
    For more readability disable the middleware by default. Active this
    middleware is now explicit.

--- lib/sidekiq/rollbar.rb
+++ lib/sidekiq/rollbar.rb
@@ -1,14 +1,8 @@
-require 'sidekiq'
-
-require "sidekiq/rollbar/version"
-require "sidekiq/rollbar/middleware"
+require 'sidekiq/util'
+require 'sidekiq/rollbar/version'
+require 'sidekiq/rollbar/middleware'
 
 module Sidekiq
   module Rollbar
-    Sidekiq.configure_server do |config|
-      config.server_middleware do |chain|
-        chain.add Sidekiq::Rollbar::Middleware
-      end
-    end
   end
 end
