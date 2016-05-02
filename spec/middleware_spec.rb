c00222a60a3ff2dc3ac23fd1204b1a22769f72e4
Gearnode <bfrimin@student.42.fr>
2016-05-02 16:58:15 +0200

    Add spec for verify middleware is not add by default

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe Sidekiq::Rollbar::Middleware do
       subject.call(worker, "message", "default") { raise exception }
     end.to raise_error(RuntimeError)
   end
+
+  it 'should not add middleware into sidekiq queue' do
+    middlewares = Sidekiq.server_middleware.entries.map(&:klass)
+    expect(middlewares).not_to include(Sidekiq::Rollbar::Middleware)
+  end
 end
